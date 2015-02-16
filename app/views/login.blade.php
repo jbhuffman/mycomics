bf717d4d693d71a27091564d0143f860be5adcf5
Jarad Huffman <jbhuffman@gmail.com>
2015-02-16 10:30:24 -0600

    updated login page layout

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -63,13 +63,17 @@
                     </div>
 
                     <div class="form-group">
-                        {{ Form::label('email', 'Email Address') }}
-                        {{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com')) }}
+                        {{ Form::label('email', 'Email Address', array('class' => 'col-sm-2 control-label')) }}
+                        <div class="col-sm-10">
+                            {{ Form::text('email', Input::old('email'), array('class' => 'form-control', 'placeholder' => 'awesome@awesome.com')) }}
+                        </div>
                     </div>
 
                     <div class="form-group">
-                        {{ Form::label('password', 'Password') }}
-                        {{ Form::password('password') }}
+                        {{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }}
+                        <div class="col-sm-10">
+                            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'password')) }}
+                        </div>
                     </div>
 
                     <p>{{ Form::submit('Submit!') }}</p>
