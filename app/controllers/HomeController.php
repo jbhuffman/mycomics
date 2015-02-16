e79e9538c8998619867a63ba4ac615c7fa90c5e0
Jarad Huffman <jbhuffman@gmail.com>
2015-02-16 10:35:43 -0600

    updated spacing, no functional change

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,62 +2,62 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |   Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+    public function showWelcome()
+    {
+        return View::make('hello');
+    }
 
-	public function showLogin()
-	{
-		// show the login form
-		return View::make('login');
-	}
+    public function showLogin()
+    {
+        // show the login form
+        return View::make('login');
+    }
 
-	public function doLogin()
-	{
-		// process the login
-		$rules = array(
-			'email' => 'required|email',
-			'password' => 'required|alphanum|min:3'
-		);
+    public function doLogin()
+    {
+        // process the login
+        $rules = array(
+            'email' => 'required|email',
+            'password' => 'required|alphanum|min:3'
+        );
 
-		$validator = Validator::make(Input::all(), $rules);
+        $validator = Validator::make(Input::all(), $rules);
 
-		if ($validator->fails()) {
-			return Redirect::to('login')
-				->withErrors($validator)
-				->withInput(Input::except('password'));
-		} else {
-			$userdata = array(
-				'email' => Input::get('email'),
-				'password' => Input::get('password')
-			);
+        if ($validator->fails()) {
+            return Redirect::to('login')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            $userdata = array(
+                'email' => Input::get('email'),
+                'password' => Input::get('password')
+            );
 
-			if (Auth::attempt($userdata)) {
-				return Redirect::intended();
-			} else {
-				return Redirect::to('login');
-			}
-		}
-	}
+            if (Auth::attempt($userdata)) {
+                return Redirect::intended();
+            } else {
+                return Redirect::to('login');
+            }
+        }
+    }
 
-	public function doLogout()
-	{
-		Auth::logout();
-		return Redirect::to('/');
-	}
+    public function doLogout()
+    {
+        Auth::logout();
+        return Redirect::to('/');
+    }
 
 }
