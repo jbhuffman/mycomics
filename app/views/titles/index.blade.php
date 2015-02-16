5ff5285b08883ed6e87afbf8ce3d77ea6b0ed5fe
Jarad Huffman <jbhuffman@gmail.com>
2015-02-16 09:58:41 -0600

    added auth checks on title list

--- app/views/titles/index.blade.php
+++ app/views/titles/index.blade.php
@@ -11,8 +11,10 @@
 
 @section('content')
     <h2>My Titles ({{ count($titles) }})</h2>
+    @if (Auth::check())
     <div class="col-md-6"><a href="{{ URL::to('titles/create') }}">Add a Title</a></div>
     <div class="col-md-6 text-right"><a href="{{ URL::to('mybooks/create') }}">Add a Book</a></div>
+    @endif
 
     <table id="titles" class="table table-striped table-condensed table-bordered">
         <thead>
@@ -35,9 +37,10 @@
                     <!-- show the title (uses the show method found at GET /titles/{id} -->
                     <a class="btn btn-small btn-success" href="{{ URL::to('titles/' . $value->id) }}">View</a>
 
+                    @if (Auth::check())
                     <!-- edit this title (uses the edit method found at GET /titles/{id}/edit -->
                     <a class="btn btn-small btn-info" href="{{ URL::to('titles/' . $value->id . '/edit') }}">Edit</a>
-
+                    @endif
                 </td>
             </tr>
         @endforeach
